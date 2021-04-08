cbb5cbaf7036141e7998a1e1434dc86d01bfba07
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-04-08 11:26:34 +0400

    added discord login

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -6,7 +6,7 @@ app = Quart(__name__)
 app.config["SECRET_KEY"] = os.environ["SPARTA_SECRET_KEY"]
 app.config["DISCORD_CLIENT_ID"] = int(os.environ["SPARTA_CLIENT_ID"])
 app.config["DISCORD_CLIENT_SECRET"] = os.environ["SPARTA_CLIENT_SECRET"]
-app.config["DISCORD_REDIRECT_URI"] = "https://127.0.0.1:5000/callback"
+app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"
 
 discord = DiscordOAuth2Session(app)
 
--- dashboard/routes.py
+++ dashboard/routes.py
@@ -1,14 +1,25 @@
-from quart import render_template, url_for
+from quart import render_template, url_for, redirect
 
-from dashboard import app
+from dashboard import app, discord
 
 
 @app.route("/")
 @app.route("/home")
 async def home():
-    return "Hello"
+    return await render_template("home.html")
+
+
+@app.route("/login")
+async def login():
+    return await discord.create_session()
 
 
 @app.route("/callback")
 async def callback():
-    return "Coming soon..."
+    try:
+        await discord.callback()
+    except:
+        return redirect(url_for("login"))
+
+    user = await discord.fetch_user()
+    return f"Hello {user}"
